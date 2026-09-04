f64b0b31be5c15cd2f41d54ddb30d7fb305269f2
Daniel Pacheco Villarpando <dennieru.com@gmail.com>
2026-09-04 06:59:44 -0400

    Fixes #35, Fixes #25: Replace non-existent mask-icon.svg with icon.svg

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icon.svg'],
       workbox: {
         navigateFallbackAllowlist: [/^\/mark-down\//],
       },
